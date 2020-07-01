591f8d9ac75097337008698e9ba7f64b05885502
udav2alex <udav2alex@gmail.com>
2020-07-01 17:44:27 +0300

    Добавил 2 класса с вебинара. Сделал первую задачу с чтением строки справа налево.

---
+++ src/ru/gressor/algorithms/lesson3/Queue.java
@@ -0,0 +1,64 @@
+package ru.gressor.algorithms.lesson3;
+
+import java.util.NoSuchElementException;
+
+public class Queue {
+    private int capacity;
+    private int[] queue;
+    private int head;
+    private int tail;
+    private int items;
+
+    public Queue(int capacity) {
+        this.capacity = capacity;
+        queue = new int[capacity];
+        head = 0;
+        tail = -1;
+        items = 0;
+    }
+
+    public boolean isEmpty() {
+        return items == 0;
+    }
+
+    public boolean isFull() {
+        return items == capacity;
+    }
+
+    public int size() {
+        return items;
+    }
+
+    public void insert(int value) {
+        if (isFull()) {
+            capacity *= 2;
+            int[] newQ = new int[capacity];
+            if (tail >= head) {
+                System.arraycopy(queue, 0, newQ, 0, queue.length);
+            } else {
+                System.arraycopy(queue, 0, newQ, 0, tail + 1);
+                System.arraycopy(queue, head,
+                        newQ, capacity - (queue.length - head),
+                        queue.length - head - 1);
+            }
+            queue = newQ;
+        }
+        if (tail == capacity - 1)
+            tail = -1;
+        queue[++tail] = value;
+        items++;
+    }
+
+    public int remove() {
+        if (isEmpty())
+            throw new NoSuchElementException("Queue is empty");
+        int temp = queue[head++];
+        head %= capacity; // if (head == capacity) head = 0;
+        items--;
+        return temp;
+    }
+
+    public int peek() {
+        return queue[head];
+    }
+}
